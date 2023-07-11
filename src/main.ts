e9283d869852dbee44e9289384dfa4fd6fecd075
Carlos Labrin <carlos.labrin15@gmail.com>
2023-07-10 22:51:02 -0400

    comit respaldo

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+// import './topics/01-basic-types';
+// import './topics/02-object-interfa';
+// import './topics/03-funcions';
+import './topics/04-homework-typer';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>Hello mundo</h1>
+`
+console.log('Hola mundo')
+
