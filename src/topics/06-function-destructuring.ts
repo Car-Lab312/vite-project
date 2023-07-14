344bfc7fa5f38965ae4be5c50dd29196642fc7d7
Carlos Labrin <carlos.labrin@hbl.local>
2023-07-14 08:07:31 -0400

    comit de respaldo 3

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -1,26 +1,26 @@
 
 
-interface Product {
-    descripction: string;
-    price: number;
-}
+// interface Product {
+//     descripction: string;
+//     price: number;
+// }
 
-const phone: Product = {
-    descripction: 'Nokia A1',
-    price: 150.0
-}
+// const phone: Product = {
+//     descripction: 'Nokia A1',
+//     price: 150.0
+// }
 
-const tablet: Product = {
-    descripction: 'iPad Air',
-    price: 250.0
-}
+// const tablet: Product = {
+//     descripction: 'iPad Air',
+//     price: 250.0
+// }
 
-function taxCalculation(): number[]{
+// function taxCalculation(): number[]{
 
-}
+// }
 
-const shoppingCart = [phone, tablet];
-const tax = 0.15;
+// const shoppingCart = [phone, tablet];
+// const tax = 0.15;
 
 
 
